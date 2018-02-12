554dcb35b79d5c1db8caa0506a52ebff97223fd8
Matt Bailey <matt@j-bailey.co.uk>
2018-02-12 13:45:56 +0000

    Apache and WP-admin

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'maximumm4tt' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'wp-database.cluster-cmwrskb4w8hv.eu-west-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'wordpress.cluster-cmwrskb4w8hv.eu-west-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
